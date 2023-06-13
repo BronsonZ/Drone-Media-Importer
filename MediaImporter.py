2561744dd48192017856558a230d44e96c00b6f8
Bronson Zell <bzell007@gmail.com>
2023-06-13 03:10:45 -0400

    All files are renamed, refactored code

--- MediaImporter.py
+++ MediaImporter.py
@@ -8,31 +8,63 @@ dest_dir = "E:\\Dropbox\\Drone Imports"
 summary_array = []
 
 def is_image(file_path):
-    img_extensions = ('.jpg', '.JPG', '.jpeg', '.JPEG', '.png', '.PNG', '.dng', '.DNG')
+    img_extensions = ('.jpg', '.JPG', '.jpeg', '.JPEG', '.dng', '.DNG')
     return file_path.endswith(img_extensions)
 
 def is_video(file_path):
     video_extensions = ('.mp4', '.MP4', '.mov', '.MOV', '.srt', '.SRT')
     return file_path.endswith(video_extensions)
 
-def import_media():
+def get_dir_size(path):
+    total = 0
+    with os.scandir(path) as it:
+        for entry in it:
+            if entry.is_file():
+                total += entry.stat().st_size
+            elif entry.is_dir():
+                total += get_dir_size(entry.path)
+    return total
+
+def copy_file(src_file_path, dest_file_path):
+    try:
+        shutil.copy2(src_file_path, dest_file_path)
+        return True
+    except (shutil.Error, FileNotFoundError) as e:
+        print("Error copying file " + src_file_path + " to " + dest_file_path + " | Error: " + str(e))
+        return False
+    
+def make_dir(path):
+    try:
+        os.makedirs(path, exist_ok=True)
+        return True
+    except OSError as e:
+        print("Error creating directory " + path + " | Error: " + str(e))
+        return False
+
+def get_creation_time(file_path):
+    creation_time = datetime.datetime.fromtimestamp(os.path.getmtime(file_path))
+    year = str(creation_time.year)
+    month = str(creation_time.month) + ' - ' + creation_time.strftime("%B")
+    day = str(creation_time.day) + ' - ' + creation_time.strftime("%A")
+    return creation_time, year, month, day
+
+def import_photos_and_videos():
     copied_files = 0
     media_src_dir = os.path.join(src_dir, "100MEDIA")
     num_files = len(os.listdir(media_src_dir))
-    print("Importing Media: " + str(num_files) + " files")
+    print("Importing Photos/Videos: " + str(num_files) + " files")
     for file in os.listdir(media_src_dir):
         src_file_path = os.path.join(media_src_dir, file)
 
-        creation_time = datetime.datetime.fromtimestamp(os.path.getmtime(src_file_path))
+        creation_time, year, month, day = get_creation_time(src_file_path)
 
-        year = str(creation_time.year)
-        month = str(creation_time.month) + ' - ' + creation_time.strftime("%B")
-        day = str(creation_time.day) + ' - ' + creation_time.strftime("%A")
+        date_string = creation_time.strftime("%Y-%m-%d-%H%M")
+        new_file_name = date_string + " - " + file
 
         if is_image(src_file_path):
-            dest_file_path = os.path.join(dest_dir, year, month, day, "Photos", file)
+            dest_file_path = os.path.join(dest_dir, year, month, day, "Photos", new_file_name)
         elif is_video(src_file_path):
-            dest_file_path = os.path.join(dest_dir, year, month, day, "Videos", file)
+            dest_file_path = os.path.join(dest_dir, year, month, day, "Videos", new_file_name)
         else:
             print("File " + file + " is not an image or video, skipping")
             num_files -= 1
@@ -42,78 +74,86 @@ def import_media():
             print("File " + dest_file_path + " already exists, skipping")
             continue
 
-        try:
-            os.makedirs(os.path.dirname(dest_file_path), exist_ok=True)
-        except OSError as e:
-            print("Error creating directory: " + str(e))
+        if not make_dir(os.path.dirname(dest_file_path)):
             continue
 
-        try:
-            print("#" + str(copied_files+1) + "/" + str(num_files) + " Copying " + file + " to " + dest_file_path)
-            shutil.copy2(src_file_path, dest_file_path)
-            print("Done copying #" + str(copied_files+1) + "/" + str(num_files) )
-            copied_files += 1
-        except shutil.Error as e:
-            print("Error copying file: " + str(e))
+        print("#" + str(copied_files+1) + "/" + str(num_files) + " Copying " + file + " to " + dest_file_path)
+        
+        if not copy_file(src_file_path, dest_file_path):
             continue
-    print("Finished, copied " + str(copied_files) + "/" + str(num_files) + " files")
+
+        print("Done copying #" + str(copied_files+1) + "/" + str(num_files) )
+        copied_files += 1
+
     summary_array.append("MEDIA: copied " + str(copied_files) + "/" + str(num_files) + " files")
+    print(summary_array[-1])
 
-def import_panorama_and_hyperlapse(dir):
-    src_dir_path = os.path.join(src_dir, dir)
+def import_panorama_or_hyperlapse(input_dir):
+    src_dir_path = os.path.join(src_dir, input_dir)
 
     copied_dirs = 0
     num_dirs = len(os.listdir(src_dir_path))
-    print("Importing: " + dir + " with " + str(num_dirs) + " directories")
+    print("Importing: " + input_dir + " with " + str(num_dirs) + " folders")
 
     for sub_dir in os.listdir(src_dir_path):
         src_sub_dir_path = os.path.join(src_dir_path, sub_dir)
 
-        creation_time = datetime.datetime.fromtimestamp(os.path.getmtime(src_sub_dir_path))
+        creation_time, year, month, day = get_creation_time(src_sub_dir_path)
 
-        year = str(creation_time.year)
-        month = str(creation_time.month) + ' - ' + creation_time.strftime("%B")
-        day = str(creation_time.day) + ' - ' + creation_time.strftime("%A")
+        date_string = creation_time.strftime("%Y-%m-%d")
+        new_dir_name = date_string + " - " + sub_dir
 
-        if dir == "PANORAMA":
-            dest_dir_path = os.path.join(dest_dir, year, month, day, "Panoramas", sub_dir)
+        if input_dir == "PANORAMA":
+            dest_dir_path = os.path.join(dest_dir, year, month, day, "Panoramas", new_dir_name)
         else:
-            dest_dir_path = os.path.join(dest_dir, year, month, day, "Hyperlapses", sub_dir)
-
-        if os.path.exists(dest_dir_path):
-            print("Directory " + dest_dir_path + " already exists, skipping")
+            dest_dir_path = os.path.join(dest_dir, year, month, day, "Hyperlapses", new_dir_name)
+        
+        if not make_dir(dest_dir_path):
             continue
 
-        try:
-            os.makedirs(os.path.dirname(dest_dir_path), exist_ok=True)
-        except OSError as e:
-            print("Error creating directory: " + str(e))
-            continue
+        print("#" + str(copied_dirs+1) + "/" + str(num_dirs) + " Copying " + sub_dir + " to " + dest_dir_path)
+        for file in os.listdir(src_sub_dir_path):
+            src_file = os.path.join(src_sub_dir_path, file)
+            new_file_name = new_dir_name + " - " + file
+            dest_file = os.path.join(dest_dir_path, new_file_name)
 
-        try:
-            print("Copying " + sub_dir + " to " + dest_dir_path)
-            shutil.copytree(src_sub_dir_path, dest_dir_path, copy_function=shutil.copy2, dirs_exist_ok=False)
-            print("Done copying " + sub_dir)
+            success = True
+
+            if os.path.exists(dest_file):
+                print("File " + dest_file + " already exists, skipping")
+                continue
+            
+            if not copy_file(src_file, dest_file):
+                success = False
+                continue
+
+        if success:
+            print("Fully copyied #" + str(copied_dirs+1) + "/" + str(num_dirs) )
             copied_dirs += 1
-        except FileExistsError as e:
-            print("Error copying directory: " + str(e))
-            continue
-        except shutil.Error as e:
-            print("Error copying directory: " + str(e))
-            continue
-    print("Finished " + dir + ", copied " + str(copied_dirs) + "/" + str(num_dirs) + " directories")
-    summary_array.append(dir + ": copied " + str(copied_dirs) + "/" + str(num_dirs) + " directories")
 
-try:
-    import_media()
-    import_panorama_and_hyperlapse("PANORAMA")
-    import_panorama_and_hyperlapse("HYPERLAPSE")
-    print("Summary:")
-    for line in summary_array:
-        print(line)
-    input("Program finished, press enter to exit")
-    exit()
-except KeyboardInterrupt:
-    print("User interrupted, closing program")
-    input("Press enter to exit")
-    exit()
+    summary_array.append(input_dir + ": fully copied " + str(copied_dirs) + "/" + str(num_dirs) + " folders")
+    print(summary_array[-1])
+
+def main():
+    try:
+        total_size = get_dir_size(src_dir)
+        print("Total size to be coppied: " + str(round(total_size/1000000000, 1)) + "GBs")
+        input("Press enter to continue, ctrl+c to exit")
+
+        import_photos_and_videos()
+        import_panorama_or_hyperlapse("PANORAMA")
+        import_panorama_or_hyperlapse("HYPERLAPSE")
+
+        print("Summary:")
+        for line in summary_array:
+            print(line)
+        
+        input("Program finished, press enter to exit")
+        exit()
+    except KeyboardInterrupt:
+        print("User interrupted, program stopped")
+        input("Press enter to exit")
+        exit()
+
+if __name__ == "__main__":
+    main()
